a130c01bb983a50f348229d4321557845ddbbde9
Benjamin Caradeuc <benjamin@caradeuc.info>
2019-08-15 22:53:17 +0200

    header responsive

--- themes/sarah-cuisine/source/js/main.js
+++ themes/sarah-cuisine/source/js/main.js
@@ -1,3 +1,24 @@
+function greatings () {
+  console.log('Bienvenue sur "Sarah cuisine"!')
+}
+
+function handleResponsiveMenu () {
+  const $navToggle = document.querySelector('.site-header .nav-toggle')
+  const $nav = document.querySelector('.site-header nav')
+
+  $navToggle.addEventListener('click', function (e) {
+    e.preventDefault()
+    $navToggle.classList.toggle('nav-visible')
+    $nav.classList.toggle('visible')
+  })
+
+  $nav.addEventListener('click', function() {
+    $navToggle.classList.remove('nav-visible')
+    $nav.classList.remove('visible')
+  })
+}
+
 (function() {
-  console.log('Sarah cuisine!')
+  greatings()
+  handleResponsiveMenu()
 })()
